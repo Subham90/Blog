2563497313c5c36e8c4b5773008811f0cdcb6a77
Subham90 <sg.21u10083@btech.nitdgp.ac.in>
2023-06-29 21:28:52 +0530

    ll

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("/posts" + search);
+      const res = await axios.get("https://blog-backend-ho92.onrender.com/posts" + search);
       setPosts(res.data);
     };
     fetchPosts();
--- client/src/components/settings/Settings.jsx
+++ client/src/components/settings/Settings.jsx
@@ -30,11 +30,11 @@ export default function Settings() {
       data.append("file", file);
       updatedUser.profilePic = filename;
       try {
-        await axios.post("/upload", data);
+        await axios.post("https://blog-backend-ho92.onrender.com/upload", data);
       } catch (err) {}
     }
     try {
-      const res = await axios.put("/users/" + user._id, updatedUser);
+      const res = await axios.put("https://blog-backend-ho92.onrender.com/users/" + user._id, updatedUser);
       setSuccess(true);
       dispatch({ type: "UPDATE_SUCCESS", payload: res.data });
     } catch (err) {
